939708a752defe6a9f6475bad1d0a7120f63bbad
Prateek <prateekkumarprasad15@gmail.com>
2026-02-24 20:15:23 +0530

    refactor: remove deprecated files and modules from Neural Network Toolbox

--- nntoolbox/cli.py
+++
@@ -1,9 +0,0 @@
-def main():
-    import sys
-    from pathlib import Path
-    from streamlit.web import cli as stcli
-
-    app_path = Path(__file__).resolve().parents[1] / "streamlit_app.py"
-
-    sys.argv = ["streamlit", "run", str(app_path)]
-    sys.exit(stcli.main())
