7a602de92dd3540e8bede700d3d4dd4dd06d35bd
Prateek <prateekkumarprasad15@gmail.com>
2026-02-24 14:10:27 +0530

    feat: add setup.py for packaging Neural Network Toolbox

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nntoolbox",
+    version="0.1.0",
+    description="Neural Network Learning Toolbox (Streamlit)",
+    author="Prateek Kumar Prasad",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "streamlit",
+        "numpy",
+        "pandas",
+        "opencv-python",
+    ],
+    entry_points={
+        "console_scripts": [
+            "nntoolbox = nntoolbox.cli:main"
+        ]
+    },
+)
